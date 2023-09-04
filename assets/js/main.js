fdf08f4ac57c76aefddb07063ffea9f1eda31335
Isabel Alves <66089395+isalves02@users.noreply.github.com>
2023-09-04 17:01:02 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,54 +1,31 @@
-var splideCarousel = new Splide( '.splide', {
-    type   : 'slide',
-    gap: 30,
-    perPage: 2,
-    drag   : true,
-    pagination: true,
-    breakpoints: {
-		768: {
-            gap: 0,
-			perPage: 1,
-		},
-    }
-}).mount();
-
-
-var menuToggle = document.querySelector('.nav .menu .menu-toggle');
-menuToggle.addEventListener('click', function() {
-    this.parentElement.classList.toggle('active');
+$(window).on("load", function() {
+    menuToggle();
 });
 
 
-var activeButtons = document.querySelectorAll('.tools .btn-group button');
-activeButtons.forEach(current => {
-    current.addEventListener('click', function() {
-        if (this.getAttribute('data-ref') == 'lang') {
-            this.classList.add('active');
-            document.querySelector('.lang-list').classList.add('active');
+$(window).on("scroll", function() {
+   showContent();
+});
 
-            if (document.querySelector('.tools-list').classList.contains('active')) {
-                document.querySelector('.tools .btn-group [data-ref="tools"]').classList.remove('active');
-                document.querySelector('.tools-list').classList.remove('active');
-            }
-        } else {
-            this.classList.add('active');
-            document.querySelector('.tools-list').classList.add('active');
-            if (document.querySelector('.lang-list').classList.contains('active')) { 
-                document.querySelector('.tools .btn-group [data-ref="lang"]').classList.remove('active');
-                document.querySelector('.lang-list').classList.remove('active'); 
-            }
-        }
+
+$(window).on("resize", function() {
+   showContent();
+
+});
+
+
+var menuToggle = function () {
+    $(".nav .menu .menu-toggle").on("click", function() {
+        $(this).parent().toggleClass("active");
     });
-});
+}
 
+var showContent = function () {
+    let vh = $(window).height();
+    var pos = $(window).scrollTop();
 
-if (window.innerWidth < 991) {
-    var showProjectsLinks = document.querySelectorAll('.splide__slide__container');
-    showProjectsLinks.forEach(project => {
-        project.addEventListener('click', function() {
-            var title = this.children[1];
-            title.classList.toggle('active');
-    
-        });
+    $('[class*="animate__"]').each(function(){
+        let limit = 50;
+        $(this).toggleClass('visible', pos + vh >= $(this).offset().top + limit);
     });
 }
